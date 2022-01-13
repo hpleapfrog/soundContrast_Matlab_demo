d975027828725bdead09d6f77ae6639292c0f90c
hpleapfrog <1394754354@qq.com>
2022-01-13 23:27:05 +0800

    更新MFcc,注释

--- soundContrast.m
+++ soundContrast.m
@@ -1,6 +1,6 @@
 function testmsg = soundContrast(testdir, n, code)
 nameList={'1','2','3','4','5','6','7','8','9' ,'10'};                        %这个是我们要识别的9个数
-for k = 1:n                     % 数据库中每一个说话人的特征
+for k = 1:n                     % 数据库中每一个语音的特征
     file = sprintf('%ss%d.wav', testdir, k);	%找出文件的路径
     [s, fs] = audioread(file);      
         
@@ -13,7 +13,7 @@ for k = 1:n                     % 数据库中每一个说话人的特征
         dist = sum(min(d,[],2)) / size(d,1);
       
         if dist < distmin
-            distmin = dist;%%这里和test函数里面一样  但多了一个具体语者的识别
+            distmin = dist;%%对语音信号的识别
             k1 = l;
         end      
     end
